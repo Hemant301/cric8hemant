1db9d626476e3cd55a3328cdd0ff2f3a52a521ac
Hemant301 <92567409+Hemant301@users.noreply.github.com>
2022-05-01 21:50:37 +0530

    login ui

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -255,7 +255,7 @@ class HomePage extends StatelessWidget {
             child: SingleChildScrollView(
               scrollDirection: Axis.vertical,
               child: SizedBox(
-                width: double.maxFinite,
+                width: MediaQuery.of(context).size.width,
                 // height: MediaQuery.of(context).size.height * 0.37,
                 child: Card(
                     color: Colors.white,
@@ -316,7 +316,7 @@ class HomePage extends StatelessWidget {
                           ),
                         ),
                         Padding(
-                          padding: const EdgeInsets.all(8.0),
+                          padding: const EdgeInsets.all(0.0),
                           child: GestureDetector(
                             onTap: () {
                               //
